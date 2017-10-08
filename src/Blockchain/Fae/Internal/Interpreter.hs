0545cfb05730c1e00ccbbc11ab3d3d8b983e2215
Ryan Reich <ryan.reich@gmail.com>
2017-10-07 18:07:38 -0700

    Change interpretTX to interpretTXs; running the interpreter only once per batch speeds things up a lot per transaction

--- src/Blockchain/Fae/Internal/Interpreter.hs
+++ src/Blockchain/Fae/Internal/Interpreter.hs
@@ -4,16 +4,17 @@ import Blockchain.Fae.Internal.Exceptions
 import Blockchain.Fae.Internal.Storage
 import Blockchain.Fae.Internal.Transaction
 
-import Data.Typeable
+import Control.Monad 
 
 import Language.Haskell.Interpreter
 
-interpretTX :: Bool -> String -> FaeStorage ()
-interpretTX isReward txSrc = do
+interpretTXs :: [(String, Bool)] -> FaeStorage ()
+interpretTXs txSrcs = do
   result <- runInterpreter $ do
-    loadModules [txSrc]
-    setImports [txSrc, "Blockchain.Fae.Internal", "Data.Sequence", "Prelude", "Data.Dynamic"]
-    interpret 
-      ("runTransaction txID pubKey " ++ show isReward ++ " (fromList inputs) body")
-      (as :: FaeStorage ())
-  either throw id result
+    loadModules $ map fst txSrcs 
+    forM txSrcs $ \(txSrc, isReward) -> do
+      setImports [txSrc, "Blockchain.Fae.Internal", "Data.Sequence", "Prelude", "Data.Dynamic"]
+      interpret 
+        ("runTransaction txID pubKey " ++ show isReward ++ " (fromList inputs) body")
+        (as :: FaeStorage ())
+  either throw sequence_ result
--- test/Interpret.hs
+++ test/Interpret.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TupleSections #-}
 import Blockchain.Fae.Internal
 import Control.Monad.State
 import qualified Data.Map as Map
@@ -8,5 +9,5 @@ main = do
   txNames <- getArgs
   (_, Storage storage) <- 
     flip runStateT (Storage Map.empty) $ 
-      getFaeStorage $ mapM (interpretTX False) txNames
+      getFaeStorage $ interpretTXs $ map (,False) txNames
   print $ Map.map (\(TransactionEntry _ _ result) -> show result) storage
