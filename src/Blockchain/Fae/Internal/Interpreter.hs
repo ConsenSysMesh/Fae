b0724234a306809f1565a2a0c79b460c67282624
Ryan Reich <ryan.reich@gmail.com>
2017-10-08 00:04:39 -0700

    Optimize Interpreter a little more

--- src/Blockchain/Fae/Internal/Interpreter.hs
+++ src/Blockchain/Fae/Internal/Interpreter.hs
@@ -6,15 +6,34 @@ import Blockchain.Fae.Internal.Transaction
 
 import Control.Monad 
 
+import Data.List
+
 import Language.Haskell.Interpreter
 
 interpretTXs :: [(String, Bool)] -> FaeStorage ()
-interpretTXs txSrcs = do
+interpretTXs txSpecs = do
   result <- runInterpreter $ do
-    loadModules $ map fst txSrcs 
-    forM txSrcs $ \(txSrc, isReward) -> do
-      setImports [txSrc, "Blockchain.Fae.Internal", "Data.Sequence", "Prelude", "Data.Dynamic"]
-      interpret 
-        ("runTransaction txID pubKey " ++ show isReward ++ " (fromList inputs) body")
-        (as :: FaeStorage ())
+    loadModules txSrcs
+    setImportsQ $ map (,Nothing) pkgModules ++ map (\s -> (s, Just s)) txSrcs
+    mapM (flip interpret (as :: FaeStorage ()) . uncurry runTX) txSpecs
   either throw sequence_ result
+
+  where
+    txSrcs = map fst txSpecs
+    pkgModules = 
+      [
+        "Prelude",
+        "Data.Dynamic",
+        "Data.Sequence",
+        "Blockchain.Fae.Internal"
+      ]
+    runTX s isReward =
+      intercalate " "
+        [
+          "runTransaction",
+          s ++ ".txID",
+          s ++ ".pubKey",
+          show isReward,
+          parens $ "fromList " ++ s ++ ".inputs",
+          s ++ ".body"
+        ]
