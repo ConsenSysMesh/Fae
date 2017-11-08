2b56e55472e1fb6ac66b6411b9c4b4f038a5a5b7
Ryan Reich <ryan.reich@gmail.com>
2017-11-07 18:55:59 -0800

    Document the server API

--- bin/FaeServer.hs
+++ bin/FaeServer.hs
@@ -1,3 +1,24 @@
+{- Server usage:
+- Parameter 'key': String (name of a private key, stored on the server)
+- Parameter 'main': file (containing 'body :: Transaction a b' definition)
+- Parameter 'other': file (containing any module)
+- Parameter 'input': (ContractID, String)
+-
+- If 'main' is not present, then the public key for the 'key' parameter
+- will be sent in the response.
+-
+- The 'main' file and any of the 'other' files can import the 'other' files
+- as modules using their local name (say, "module M" is imported as "import
+- M" in 'main').  From the 'main' or 'other' file of any other transaction,
+- they are imported qualified by "Blockchain.Fae.TX<txID>" (say, "module M"
+- in transaction 0a1f34b9 is imported as
+- "Blockchain.Fae.Transactions.TX0a1f34b9.M" from another transaction).
+- The module declarations are automatically corrected to allow this; you
+- upload them with just the local names.
+-
+- The 'input' parameters are parsed and considered in order as the
+- transaction input list.
+-}
 import Blockchain.Fae.Internal hiding ((<.>))
 
 import Control.Concurrent
