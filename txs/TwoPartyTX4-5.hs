5ef104a5105c3223a51e682d6b9a89971ff02212
Ryan Reich <ryan.reich@gmail.com>
2017-12-07 11:37:41 -0800

    Consolidate TwoParty transactions 4 and 5

--- txs/TwoPartyTX4-5.hs
+++ txs/TwoPartyTX4-5.hs
@@ -1,3 +1,4 @@
 body :: Transaction (Maybe (Either (Versioned String) (Versioned String))) String
+body (Just (Left (Versioned s))) = return s
 body (Just (Right (Versioned s))) = return s
-body _ = error "Swap failed!"
+body _ = return ""
--- txs/TwoPartyTX5.hs
+++
@@ -1,3 +0,0 @@
-body :: Transaction (Maybe (Either (Versioned String) (Versioned String))) String
-body (Just (Left (Versioned s))) = return s
-body _ = error "Swap failed!"
