9232400a678b1ea3c0c6c9e55fcdbda3f67c9c59
Ryan Reich <ryan.reich@gmail.com>
2018-01-23 22:28:22 -0800

    Expand postTX.sh to accept environment variables in `docker run` syntax.  Remove postTX.cmd from the image for now.

--- docker/postTX/etc/postTX.sh
+++ docker/postTX/etc/postTX.sh
@@ -8,6 +8,17 @@ if [[ -n $FAE_REPO ]]
 then imgName=$FAE_REPO/$imgName
 fi
 
+envList=""
+while true; do
+  case $1 in
+    -e|--env-list|--env-file)
+      envList="$envList $1 $2"
+      shift 2
+      ;;
+    *) break ;;
+  esac
+done
+
 cd $(dirname $1)
 txfile=$(basename $1)
 shift
@@ -16,5 +27,5 @@ docker run \
   --rm \
   --network host \
   --mount type=bind,src=$PWD,dst=/txs/,readonly \
-  $imgName $txfile $@
+  $envList $imgName $txfile $@
 
