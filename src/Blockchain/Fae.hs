263d6ed3870646223bb7dba0991a5477872956bd
Ryan Reich <ryan.reich@gmail.com>
2018-02-13 21:58:43 -0800

    Add WithEscrows to Blockchain.Fae

--- src/Blockchain/Fae.hs
+++ src/Blockchain/Fae.hs
@@ -64,7 +64,7 @@ module Blockchain.Fae
     Transaction, TransactionM, TransactionID, PublicKey, FaeTX, MonadTX,
     -- * Contracts and escrows
     Contract, ContractM, ContractID(..), Fae, MonadContract,
-    EscrowID, BearsValue, RewardEscrowID, 
+    WithEscrows, EscrowID, BearsValue, RewardEscrowID, 
     spend, release, useEscrow, newEscrow, newContract,
     lookupSigner, signer, Blockchain.Fae.signers, claimReward, bearer, 
     -- * Versioning
