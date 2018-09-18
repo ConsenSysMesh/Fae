7b58547ed7df38ac8bc67a32ee5c4c6d1b11d007
Ryan Reich <ryan.reich@gmail.com>
2018-09-18 09:10:18 -0700

    Fix Secret contract

--- txs/Secret.hs
+++ txs/Secret.hs
@@ -2,7 +2,5 @@ import Blockchain.Fae.Contracts
 
 data Secret = Secret deriving (Generic, Show)
 
-body :: Transaction Void PublicKey
-body _ = do
-  deposit Secret "self"
-  signer "self"
+body :: FaeTX ()
+body = deposit Secret "self"
