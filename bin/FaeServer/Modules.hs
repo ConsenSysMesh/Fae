48222424c95acff4424eab28868f84b6dbce3eb8
Ryan Reich <ryan.reich@gmail.com>
2018-10-08 22:19:18 -0700

    Drop private import paths; have faeServer modify all imports of local modules (as specified in the transaction itself) to be qualified.

--- bin/FaeServer/Modules.hs
+++ bin/FaeServer/Modules.hs
@@ -3,6 +3,7 @@ module FaeServer.Modules where
 import Blockchain.Fae.FrontEnd
 
 import Control.DeepSeq
+import Control.Monad
 
 import qualified Data.ByteString.Lazy.Char8 as LC8
 import qualified Data.ByteString.Char8 as C8
@@ -10,8 +11,10 @@ import qualified Data.ByteString.Char8 as C8
 import Data.Map (Map)
 import qualified Data.Map as Map
 
+import Data.Char
 import Data.List
 import Data.Maybe
+import Data.Monoid
 import Data.Proxy
 import Data.Serialize
 
@@ -28,8 +31,9 @@ makeFilesMap ::
   (Serialize a) => 
   TXMessage a -> Module -> ModuleMap -> Bool -> Bool -> (TX, Module, ModuleMap)
 makeFilesMap txMessage mainFile0 modules0 reward isFake = (tx, mainFile, modules) where
-  mainFile = addHeader txID mainFile0
-  modules = Map.mapWithKey (fixHeader txID . dropExtension) modules0
+  moduleNames = dropExtension <$> Map.keys modules0
+  mainFile = fixImports txID moduleNames $ addHeader txID mainFile0
+  modules = Map.mapWithKey (fixModule txID moduleNames . dropExtension) modules0
   tx@TX{..} = 
     maybe (error "Invalid transaction message") force $
     txMessageToTX reward txMessage isFake
@@ -50,26 +54,43 @@ getFiles files name =
 
 writeModules :: Module -> ModuleMap -> TransactionID -> IO ()
 writeModules mainFile modules txID = do
-  let
-    thisTXDir = foldr (</>) "" $ mkTXPathParts txID
-    thisTXPrivate = mkTXPrivatePath txID
-    writeModule fileName fileContents = do
-      C8.writeFile (thisTXDir </> fileName) fileContents
-      C8.writeFile (thisTXPrivate </> fileName) $ privateModule txID fileName
-  createDirectoryIfMissing True thisTXPrivate
+  let thisTXDir = foldr (</>) "" $ mkTXPathParts txID
+      writeModule fileName fileContents = 
+        C8.writeFile (thisTXDir </> fileName) fileContents
+  createDirectoryIfMissing True thisTXDir
   C8.writeFile (thisTXDir <.> "hs") mainFile
   sequence_ $ Map.mapWithKey writeModule modules
-
-privateModule :: TransactionID -> String -> Module
-privateModule txID fileName = moduleHeader moduleName (Just exports) realModuleName
-  where
-    moduleName = takeBaseName fileName
-    exports = ["module " ++ realModuleName]
-    realModuleName = qualify txID moduleName
 
 addHeader :: TransactionID -> Module -> Module
 addHeader txID = C8.append header where 
   header = moduleHeader (mkTXModuleName txID) Nothing "Blockchain.Fae"
+
+fixModule :: TransactionID -> [String] -> String -> Module -> Module
+fixModule txID moduleNames fileName =
+  fixImports txID moduleNames .
+  fixHeader txID fileName
+
+fixImports :: TransactionID -> [String] -> Module -> Module
+fixImports txID moduleNames = 
+  C8.unlines . fmap (fixImport txID moduleNames) . C8.lines
+
+fixImport :: TransactionID -> [String] -> C8.ByteString -> C8.ByteString
+fixImport txID moduleNames line
+  | ("import", rest0) <- C8.break isSpace line,
+    let rest = C8.dropWhile isSpace rest0
+        (isQualified, rest') = 
+          maybe (False, rest) ((True,) . C8.dropWhile isSpace) $ 
+          C8.stripPrefix "qualified" rest
+        (moduleNameBS, rest'') = C8.break isSpace rest'
+        moduleName = C8.unpack moduleNameBS,
+    moduleName `elem` moduleNames
+    = C8.pack 
+        (
+          "import " ++ if isQualified then "qualified " else "" ++ 
+          qualify txID moduleName
+        ) <> 
+      rest''
+  | otherwise = line
 
 fixHeader :: TransactionID -> String -> Module -> Module
 fixHeader txID fileName = replaceModuleNameWith (qualify txID fileName)
